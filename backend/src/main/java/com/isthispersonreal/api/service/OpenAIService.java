70a479e3691ff9ef171b1309119848a1ee7f60c3
kabir-cs <kabirs@usf.edu>
2025-07-07 20:52:41 -0400

    style: reorder imports in OpenAIService for code consistency

--- backend/src/main/java/com/isthispersonreal/api/service/OpenAIService.java
+++ backend/src/main/java/com/isthispersonreal/api/service/OpenAIService.java
@@ -1,9 +1,9 @@
 package com.isthispersonreal.api.service;
 
-import com.isthispersonreal.api.model.AnalysisResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import com.isthispersonreal.api.model.AnalysisResult;
 
 import java.util.Map;
 
